2c4e47960daec11b3e2df2680ef0f4d32dae78f0
Melanija <melanija.sibakovska1@gmail.com>
2022-03-31 17:50:17 +0300

    if and for

--- js/05-if.js
+++ js/05-if.js
@@ -4,6 +4,10 @@ IF
 Sablonai:
 - if () {}
 - if () {} else {}
+- if () {} else if {}
+- if () {} else if {} else {}
+- if () {} else if {} ...  else if {}
+- if () {} else if {} ...  else if {} else {}
 
 Palyginimo operatoriai:
 - visi: <, >, <= , >=, ==, != (nera lygu), === (griezta ligybe), !== (griezta neligybe)
@@ -25,16 +29,72 @@ const c = 2;
 const d = 5;
 
 //jei yra tinkama salyga if'e as ilysiu i console ir isspausdinsiu kas yra viduje
-if (a > b) {
-    console.log('C nera daugiau uz D')
-}
-
-
-const e = 7;
-const f = 55;
-
-if (e > f) {
-    console.log('text')
+if (a !== b) {
+    console.log('A yra lygu B')
 } else {
-    console.log('no text')
+    console.log('A nera lygu B');
 }
+
+console.log('-----------------');
+const day = 4;
+
+if (day === 1) {
+    console.log('pirmadienis');
+} else if (day === 2) {
+    console.log('antradienis');
+} else if (day === 3) {
+    console.log('treciadienis');
+} else if (day === 4) {
+    console.log('ketvirtadienis');
+} else if (day === 5) {
+    console.log('penktadienis');
+} else if (day === 6) {
+    console.log('sestadienis');
+} else if (day === 7) {
+    console.log('sekmadienis');
+} else {
+    console.log('tokia diena savaiteje neegzistuoja');
+}
+
+console.log('-------------');
+const akiuSpalva = 'raudona';
+
+if (akiuSpalva === 'zalia') {
+    console.log('Zaliaakiai yra zalciai!');
+} else {
+    if (akiuSpalva === 'melyna') {
+        console.log('Melynakiai megsta melynes.');
+    } else {
+        if (akiuSpalva === 'ruda') {
+            console.log('Rudaakiai yra melagiai.');
+        } else {
+            if (akiuSpalva === 'raudona') {
+                console.log('Raudonakis?.. Tu turbut programuotojas...');
+            } else {
+                console.log('O tu turi akis?..');
+            }
+        }
+    }
+}
+
+console.log('-------------');
+
+const temperatura = -25;
+const nuoKadaLaikomeJogSilta = 15;
+const arYraKrituliu = false;
+
+if (arYraKrituliu) {
+    // siuo metu lyja
+    if (temperatura >= nuoKadaLaikomeJogSilta) {
+        console.log('Varyk su maike 🦈🦈');
+    } else {
+        console.log('Tau reikes kailiniu 🎅');
+    }
+} else {
+    // siuo metu ne lyja
+    if (temperatura >= nuoKadaLaikomeJogSilta) {
+        console.log('Gali eiti nuogas 🌞');
+    } else {
+        console.log('Be striukes neapsieisi ❄');
+    }
+} 
